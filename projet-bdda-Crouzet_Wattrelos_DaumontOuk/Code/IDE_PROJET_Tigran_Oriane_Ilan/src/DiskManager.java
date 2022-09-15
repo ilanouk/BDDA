2a488c2ff329c368a6d07bf9413a3770659d79b3
ilanouk <ilandaumontouk@gmail.com>
2022-09-16 01:37:55 +0200

    bla

---
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/DiskManager.java
@@ -0,0 +1,28 @@
+
+public class DiskManager {
+	
+	//Allouer une page
+	public static PageId AllocPage() {
+		
+	}
+	
+	//Remplire la page avec l'argument buff
+	public static void ReadPage(PageId pageId, buff) {
+		
+	}
+	
+	//Ecrit le contenu de l'argument buff dans le fichier
+	public static void WritePage(PageId pageId, buff) {
+		
+	}
+	
+	//Désalloue une page
+	public static void DeallocPage(PageId pageId) {
+		
+	}
+	
+	//Retourne le nb de pages allouées au disk manager
+	public static int GetCurrentAllocPages() {
+		
+	}
+}
