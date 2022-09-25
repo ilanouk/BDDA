00e764e51eeedba0294156bac95081c9a9baed99
MrGran1 <tigran.wattrelos@gmail.com>
2022-09-25 18:44:03 +0200

    AllocPage

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/DiskManager.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/DiskManager.java
@@ -1,33 +1,47 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.nio.*;
 public class DiskManager {
+	
+
+	static int numFichier = 0;
+	static ArrayList<PageId> tabPageLibre = new ArrayList<PageId>(); //tableau qui stock la liste des pages libres
 	
 	//Allouer une page
 	public static PageId AllocPage() {
 		
-	}
-		int i;
+		
 		boolean isAllouee = false;
-		while (isAllouee) {
-			String nomFichier = "F"+i+".bdda";
-			File fichier = new File(nomFichier); // on verifie que le fichier existe
-			if (fichier.exists()) {
-				//verifier  si toute les page sont alloué
-				//si oui on incremente i
-				//si non on alloue la page et on met isAllouee à Vrai
-			}
-			else {
-				//Création d'un nouveau fichier et d'un nouveau PageIDD
-				//DataOutputStream out = new DataOutputStream( new BufferedOutputStream( new FileOutputStream(nomFichier)));
-				
-				//ByteBuffer buff = ByteBuffer.allocate(DBParams.pageSize);
-				//buff.put("XXX".getBytes());
-				//isAllouee = true;
-				//allouer la premiere page et mettre isAllouee à Vrai
-				
-			}
+		
+		
+		if (tabPageLibre.size() == 0) { // si le tableau des page Libre est vide alors on crée un nouveau fichier
+			String nomFichier = "F"+numFichier+".bdda";
+			//File fichier = new File(nomFichier); // Il faut trouver un moyen de ranger le fichier dans le dossier DB
+			
+			tabPageLibre.add( new PageId(numFichier,2));
+			tabPageLibre.add( new PageId(numFichier,3));
+			numFichier +=1;
+			
+			return new PageId(numFichier,1);
+			
+		}
+		else {
+			return tabPageLibre.remove(0); 
+		}
+			
 	}
-	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	//Remplire la page avec l'argument buff
 	public static void ReadPage(PageId pageId, buff) {
 		
--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/PageId.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/PageId.java
@@ -4,4 +4,9 @@ public class PageId {
 	private int FileIdx;
 	private int PageIdx;
 	
+	
+	public PageId(int FileIdx,int PageIdx) {
+		this.FileIdx = FileIdx;
+		this.PageIdx = PageIdx;
+	}
 }
