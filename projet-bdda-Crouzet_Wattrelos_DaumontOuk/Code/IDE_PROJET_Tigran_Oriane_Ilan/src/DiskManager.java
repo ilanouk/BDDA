338e50d91b986112b678eaab303f161e9243c391
WATTRELOS TIGRAN <im08375@pc526-8.ens.math-info.univ-paris5.fr>
2022-09-26 14:50:39 +0200

    allocPage

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/DiskManager.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/DiskManager.java
@@ -10,26 +10,30 @@ public class DiskManager {
 	//Allouer une page
 	public static PageId allocPage() {
 		
-		
-		boolean isAllouee = false;
-		
-		
+		boolean isAllouee = false
 		if (tabPageLibre.size() == 0) { // si le tableau des page Libre est vide alors on crée un nouveau fichier
-			String nomFichier = "F"+numFichier+".bdda";
-			File fichier = new File("../../DB/"+nomFichier); // Il faut trouver un moyen de ranger le fichier dans le dossier DB
-			
-			tabPageLibre.add( new PageId(numFichier,2));
-			tabPageLibre.add( new PageId(numFichier,3));
-			numFichier +=1;
-			
-			return new PageId(numFichier,1);
-			
-		}
+			while(!isAllouee){
+				String nomFichier = "F"+numFichier+".bdda";
+				File fichier = new File("../../DB/"+nomFichier); // Il faut trouver un moyen de ranger le fichier dans le dossier DB
+				if (!fichier.exist()){
+					tabPageLibre.add( new PageId(numFichier,2));
+					tabPageLibre.add( new PageId(numFichier,3));
+					numFichier +=1;
+					
+					return new PageId(numFichier,1);
+				}
+				
+				
+			}
+
+
 		else {
 			return tabPageLibre.remove(0); 
 		}
-			
+				
 	}
+
+			
 	
 	//Remplire la page avec l'argument buff
 	public static void readPage(PageId pageId, ByteBuffer buff) {
