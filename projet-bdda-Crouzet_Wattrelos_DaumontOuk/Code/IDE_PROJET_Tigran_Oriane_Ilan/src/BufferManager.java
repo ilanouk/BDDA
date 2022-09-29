b060bc3ed71e93e05b8d76d0834776c1c0e4a5ad
Ilan <106468200+ilanouk@users.noreply.github.com>
2022-09-29 13:47:50 +0200

    BufferManager

---
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/BufferManager.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.nio.Buffer;
+
+public class BufferManager {
+    
+    private int pin_count=0; //Nb d'utilisateurs sur la page
+    private boolean valdirty=false; //Vaut true si page modifiée
+    //%%%%%%%%%%%%
+    private byte[] buff = DiskManager.getBuffer(); 
+
+    //Retourne un des buffers associés à une frame
+    public byte[] GetPage(PageId pageId){
+        //Buffer = contenu page désignée par pageId
+        // recuperer le buffer de diskmanager
+        //S'occuper du remplacement (LRU,clock) du contenu d'une frame
+        
+        
+        for(int i=0;i<DBParams.maxPagesPerFile;i++){ //On parcoure toutes les pages
+            for(int j=0;j<DBParams.pageSize;j++){ // On parcoure tous les fichiers de chaque pages
+                //%%%%%%%%%%%%
+                if( !DiskManager.pageIdExiste(pageId) && pin_count==0){ //Si la page n'existe pas et aucun utilisateur dessus
+                    if(valdirty==true){
+                        //ecrire contenu sur disque
+                        //valdirty=false;
+                    }
+                }
+                else{
+                    //choisir fichier libre
+                    //lire contenu page demandée dans le fichier
+                }
+                //pin_count++;
+            }
+        }
+        return buff;
+    }
+
+    //Décrémente pin_count et actualise flag dirty de la page
+    public void FreePage(PageId pageId, boolean valdirty){
+        //Peut aussi actualiser infos sur la politique de remplacement
+    }
+
+    //Ecriture des pages modifiées et remise a 0 des flags/contenus dans buffers
+    public void FlushAll(){
+        //Ecriture des pages où flag dirty=true sur disque
+        //Remise à 0 de tous les flags/infos et contenus des buffers
+        //Rajouter un appel a la methode, dans la méthode Finish du DBManager
+        
+    }
+
+}
