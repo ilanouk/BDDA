2165346d34b9df3b3c03ab996feb300b2538a7bf
Ilan <106468200+ilanouk@users.noreply.github.com>
2022-09-29 13:49:22 +0200

    Modification PageId

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/PageId.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/PageId.java
@@ -3,11 +3,15 @@ public class PageId {
 	
 	private int FileIdx;
 	private int PageIdx;
+	private int pin_count;
+	private boolean valdirty;
 	
 	
 	public PageId(int FileIdx,int PageIdx) {
 		this.FileIdx = FileIdx;
 		this.PageIdx = PageIdx;
+		this.pin_count=0;
+		this.valdirty=false;
 	}
 	
 	public int getFile(){
