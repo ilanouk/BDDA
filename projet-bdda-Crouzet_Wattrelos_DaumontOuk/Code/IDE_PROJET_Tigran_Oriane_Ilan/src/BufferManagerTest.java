9391de2c6f73096d477cfb0b1c208d1eafb11025
Ilan <106468200+ilanouk@users.noreply.github.com>
2022-10-09 16:17:34 +0200

    BMTest

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/BufferManagerTest.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/BufferManagerTest.java
@@ -11,28 +11,47 @@ public class BufferManagerTest {
      * 
      */
 
-
     //Test des fonctions getPage, freePage et flushAll
-    public static byte[] getPageTest() throws IOException{
+    public static void getPageTest() throws IOException{
 
-        //Allouer 2 pages
-        PageId page1 = DiskManager.getLeDiskManager().allocPage();
-        PageId page2 = DiskManager.getLeDiskManager().allocPage();
-        
-        //Créer contenu des bytes des pages
-        byte[] contenu1 = "C'est la 1".getBytes();
-        byte[] contenu2 = "C'est la 2".getBytes();
+        //Créer les instances
+        DiskManager dMTest = DiskManager.getLeDiskManager();
+        PageId pageTest = dMTest.allocPage();
+        BufferManager bMTest = BufferManager.leBufferManager();
+        byte[] buffTest = bMTest.getPage(pageTest);
 
-        //Ecrire les bytes dans les pages
-        DiskManager.getLeDiskManager().writePage(page1, contenu1);
-        DiskManager.getLeDiskManager().writePage(page2, contenu2);
-
-        //Mettre sur allPages les pages 1 et 2
-        byte[] allPages = BufferManager.leBufferManager().getPage(page1);
-        allPages = BufferManager.leBufferManager().getPage(page2);
-
-        return allPages;
+        //On affiche le buffer de la page test
+        System.out.println(buffTest);
     }
+
+    public static void freePageTest() throws IOException{
+
+        //Créer les instances
+        DiskManager dMTest = DiskManager.getLeDiskManager();
+        PageId pageTest = dMTest.allocPage();
+        BufferManager bMTest = BufferManager.leBufferManager();
+
+        //Créer page dans bufferManager
+        bMTest.getPage(pageTest);
+
+        System.out.println("PageId : "+ pageTest);
+        System.out.println("BufferPool avant : "+ bMTest.getBufferPool());
+        
+        bMTest.freePage(pageTest, true);
+        System.out.println("BufferPool après : "+ bMTest.getBufferPool());
+    }
+
+    public static void flushAllTest() throws IOException{
+        // *********** A FINIR ************
+         //Créer les instances
+         DiskManager dMTest = DiskManager.getLeDiskManager();
+         PageId pageTest = dMTest.allocPage();
+         BufferManager bMTest = BufferManager.leBufferManager();
+         byte[] buffTest = bMTest.getPage(pageTest);
+
+         
+    }
+
 
     public static void main(String[] args) throws IOException {
 		DiskManager.recupTabPageLibre();
@@ -42,9 +61,8 @@ public class BufferManagerTest {
 		DBParams.pageSize = 4096;
         DBParams.frameCount=2;
 
-        System.out.println(getPageTest());
-
-
+        getPageTest();
+        freePageTest();
+        flushAllTest();
     }
-
 }
