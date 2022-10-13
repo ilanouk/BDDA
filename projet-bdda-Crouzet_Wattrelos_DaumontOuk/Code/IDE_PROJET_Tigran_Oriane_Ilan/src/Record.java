d4706c5116944b53c408c03f8d60d74743cd6337
OrianeCrouzet <oriane.crouzet78@gmail.com>
2022-10-13 13:02:21 +0200

    modif + debut record

---
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/Record.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.nio.ByteBuffer;
+
+public class Record {
+    // Attributs
+    private RelationInfo relInfo;
+    private ArrayList<String> values;
+
+    // Constructeur
+    public Record(RelationInfo relInfo) {
+        this.relInfo = relInfo;
+        values = new ArrayList<String>();
+    }
+
+    // Méthodes
+    public void writeToBuffer(ByteBuffer buff, int pos) {
+        buff.position(pos);
+        for (int i = 0; i < values.size(); i++) {
+            String type = relInfo.getTypeColonne()[i].toLowerCase();
+            switch(type){ //On définit trois façons de remplir le buffer selon le type de la colonne
+                case "int" : 
+                    int resInt = Integer.valueOf(values.get(i));
+                    buff.putInt(resInt);
+                    break;
+                case "float" :
+                    float resFloat = Float.valueOf(values.get(i));
+                    buff.putFloat(resFloat);
+                    break;
+                default : 
+                    if(type.startsWith("string")){
+                        len = 
+                    }
+            }
+
+        }
+    }
+
+    public void readFromBuffer(ByteBuffer buff, int pos) {
+
+    }
+}
