022e42777e0824c421a6c779038adf662f7982c3
Ilan <106468200+ilanouk@users.noreply.github.com>
2022-10-23 11:51:06 +0200

    FileManager
    implémentation des instances

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/FileManager.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/FileManager.java
@@ -1,12 +1,19 @@
 import java.io.IOException;
 
 public class FileManager {
+	
 	private static FileManager leFileManager=new FileManager();
+	
+	//allocation d’une nouvelle page via AllocPage du DiskManager et écriture dans la page allouée
 	public static PageId CreateNewHeaderPage() throws IOException
 	{
-		PageId n = DiskManager.getLeDiskManager().allocPage();
+		//Création des instances
+		PageId pageId = DiskManager.getLeDiskManager().allocPage();
+		DiskManager diskM = DiskManager.getLeDiskManager();
+        BufferManager buffM = BufferManager.getLeBufferManager();
+		byte[] buffer = buffM.getPage(pageId);
 		
-		return n;
+		return pageId;
 		// pas fini
 	}
 	public static PageId addDataPage(RelationInfo relInf) throws IOException {
