89a9ad47b9c1c699aeaf578d9982160c4268059d
MrGran1 <tigran.wattrelos@gmail.com>
2022-10-26 18:18:16 +0200

    processCommand Create Table

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/CreateTableCommand.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/CreateTableCommand.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
+
+
 public class CreateTableCommand{
     private String nomRelationInfo;
     private int nbrColonnes;
-    private String nomColonne;
-    private String typeColonne;
+    private ArrayList<ColInfo> colonnes = new ArrayList<ColInfo>();
 
+
+
+
+    public CreateTableCommand(String nomRelationInfo,int nbrColonne,ArrayList<ColInfo>colonnes){
+        this.colonnes=colonnes;
+        this.nbrColonnes=nbrColonne;
+        this.nomRelationInfo=nomRelationInfo;
+
+    }
     public void execute(){
         PageId pageid = FileManager.createNewHeaderPage();
-        RelationInfo relation = new RelationInfo(nomRelationInfo,nbrColonnes,nomColonne,typeColonne);
+        RelationInfo relation = new RelationInfo(nomRelationInfo,nbrColonnes,colonnes);
         Catalog.getLeCatalog().addRelationInfo(relation);
     }
 }
