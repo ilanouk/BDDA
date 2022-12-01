bd7bf87be2ae177cc624cc35a965b8ae6da73a4f
CROUZET ORIANE <il01786@migre-1.ens.math-info.univ-paris5.fr>
2022-12-01 14:21:14 +0100

    tp4

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/RecordTest.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/RecordTest.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class RecordTest {
     public static void main(String[] args) throws IOException {
         ArrayList<ColInfo> liste = new ArrayList<ColInfo>();
-        liste.add(new ColInfo("Nom", "varchar(10)"));
-        liste.add(new ColInfo("Prenom", "varchar(10)"));
-        liste.add(new ColInfo("Age", "integer"));
-        liste.add(new ColInfo("Note", "real"));
+        liste.add(new ColInfo("Nom", "VARCHAR(10)"));
+        liste.add(new ColInfo("Prenom", "VARCHAR(10)"));
+        liste.add(new ColInfo("Age", "INTEGER"));
+        liste.add(new ColInfo("Note", "REAL"));
         PageId id = new PageId(0, 0);
         RelationInfo relI = new RelationInfo("etudiant", liste, id);
         Record rec = new Record(relI);
@@ -21,9 +21,10 @@ public class RecordTest {
         System.out.println(rec.getWrittenSize());
         ByteBuffer buff = ByteBuffer.allocate(100);
         rec.writeToBuffer(buff, 0);
-        // buff.position(0);
         for (int j = 0; j < 5; j++) {
             System.out.println("buff " + j + " " + buff.getInt());
         }
+        
+        rec.readFromBuffer(buff, 0);
     }
 }
