4670a9f2a4b5d9ed29bdc2e653124f4d7aa77987
Ilan <106468200+ilanouk@users.noreply.github.com>
2022-12-05 14:08:46 +0100

    FileManager

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/FileManager.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/FileManager.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class FileManager {
 	
@@ -50,17 +49,6 @@ public class FileManager {
 		
 		return pageId;
 	}
-	
-
-	//PAS BON
-	// Pour la relation désignée par relInfo, ca renvoie le pageId où il reste assez de place pour insérer le record
-	// public PageId getFreeDataPageId(RelationInfo relInfo, int sizeRecord) throws IOException{
-
-	// 	PageId pageIdHeaderPage = relInfo.getHeaderPageId();
-	// 	HeaderPage hP = new HeaderPage(pageIdHeaderPage);
-
-	// 	return hP.getDPEnoughSpace(sizeRecord);
-	// }
 
 	//PRESQUE
 	//Ecrit l'enregistrement record dans la data page de pageId & renvoie son recordId
@@ -76,24 +64,20 @@ public class FileManager {
 		
 	}
 
-	// A FINIR !!!
+	//A VERIFIER
 	//Renvoie la liste des records stockés dans la pageId
 	public ArrayList<Record> getRecordsInDataPage(RelationInfo relInfo, PageId pageId) throws IOException{
 		BufferManager buffM = BufferManager.getLeBufferManager();
 		HeaderPage hP = new HeaderPage(pageId);
+		ArrayList<Record> records = new ArrayList<Record>();
 		Record record = new Record(relInfo);
-		ArrayList<Record> allRecord = new ArrayList<Record>();
-		int idx=0;
-
-		// while(allRecord.get(idx)!=null){
-		// 	idx++;
-		// }
-
-		record.readFromBuffer(hP.gByteBuffer(), pageId.getPage());
-		allRecord.add(idx, record);
-
-		return allRecord;
 		
+		record.readFromBuffer(hP.gByteBuffer(), pageId.getPage());
+		records.add(record);
+		
+		buffM.freePage(pageId, false);
+		
+		return records;
 	}
 
 	//A VERIFIER
@@ -110,34 +94,24 @@ public class FileManager {
 
 		return allPageId;
 	}
-
-	// // A FINIR !!!
-	// //Renvoie les pageId des pages de données, ceux de la HeaderPage
-	// //Ici relInfo permet d'avoir le type (integer, real)
-	// public ArrayList<PageId> getAllDataPages (RelationInfo relInfo){
-	// 	ArrayList<PageId> dataPages = new ArrayList<PageId>();
-
-		
-
-	// 	return dataPages;
-	// }
-
 	
-	//OK 
+	//A VERIFIER 
 	//Insertion d'un record dans une relation
 	public RecordId InsertRecordIntoRelation (Record record) throws IOException {
 		int recSize = record.getWrittenSize();
-		return writeRecordToDataPage(record, getFreeDataPageId(record.getRelInfo(), recSize)); 
+		return this.writeRecordToDataPage(record, getFreeDataPageId(record.getRelInfo(), recSize)); 
 	}
 
-	//OK
+	//A VERIFIER
 	//Lister tous les records dans une relation
 	public ArrayList<Record> GetAllRecords (RelationInfo relInfo) throws IOException{
 		ArrayList<Record> allRecord = new ArrayList<Record>();
 		ArrayList<PageId> allPageId = getAllPageId(relInfo);
+		int i=0;
 		while (allPageId.size()!=0) {
-			allRecord.addAll(getRecordsInDataPage(relInfo, allPageId.get(0)));
-			allPageId.remove(0);
+			allRecord.addAll(getRecordsInDataPage(relInfo, allPageId.get(i)));
+			allPageId.remove(i);
+			i++;
 		}
 		return allRecord;
 	}
