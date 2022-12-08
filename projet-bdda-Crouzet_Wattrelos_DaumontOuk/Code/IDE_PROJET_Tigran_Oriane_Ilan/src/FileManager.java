6b3d47610e372e9cc7330629b29d31967c7ffff5
Ilan <106468200+ilanouk@users.noreply.github.com>
2022-12-08 21:08:09 +0100

    FM Modif

--- projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/FileManager.java
+++ projet-bdda-Crouzet_Wattrelos_DaumontOuk/Code/IDE_PROJET_Tigran_Oriane_Ilan/src/FileManager.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 public class FileManager {
@@ -9,7 +10,6 @@ public class FileManager {
 		return leFileManager;
 	}
 	
-	//OK
 	//allocation d’une nouvelle page via AllocPage du DiskManager et écriture dans la page allouée
 	public PageId createNewHeaderPage() throws IOException {
 		//Création des instances
@@ -24,7 +24,6 @@ public class FileManager {
 		return pageId;
 	}
 
-	//OK
 	//Ajoute une page de données vide au Heap File correspondant à la relation relInfo
 	public PageId addDataPage(RelationInfo relInf) throws IOException {
 		//Création des instances
@@ -40,30 +39,74 @@ public class FileManager {
 	public PageId getFreeDataPageId( RelationInfo relInf, int sizeRecord) throws Exception {
 		//Création des instances
 		BufferManager buffM = BufferManager.getLeBufferManager();
-		HeaderPage hP = new HeaderPage(relInf.getHeaderPageId());
-		PageId pageId = hP.getDPEnoughSpace(sizeRecord);
+		PageId pageIdHp = relInf.getHeaderPageId();
+		ByteBuffer buffer = buffM.getPage(pageIdHp);
+		PageId pageId = readPageIdFromPageBuffer(buffer, true);
+
+		if(pageId.getFile()==-1){
+			return this.addDataPage(relInf);
+		}
 		
 		//Libérer page allouée auprès du Buffer Manager
-		buffM.freePage(pageId, false);
+		buffM.freePage(pageIdHp, false);
 		
 		return pageId;
 	}
 
-	//PRESQUE
 	//Ecrit l'enregistrement record dans la data page de pageId & renvoie son recordId
 	public RecordId writeRecordToDataPage (Record record, PageId pageId) throws IOException, IndexOutOfBoundsException{
-		BufferManager buffM = BufferManager.getLeBufferManager();
-		HeaderPage hP = new HeaderPage(pageId);
-		RecordId recordId = new RecordId(pageId, 0);
+		BufferManager bm = BufferManager.getLeBufferManager();
+		ByteBuffer headerPage = bm.getPage(pageId);
+		ByteBuffer bufCurPageId = bm.getPage(pageId);
+		int freeSlot = 0, freePos = 0, cursor = 16 ; ////////// relinfo.getHeaderPageId
 
-		record.writeToBuffer(hP.gByteBuffer(), pageId.getPage());
-		buffM.freePage(pageId, true);
-		
-		return recordId;
+		for(int i = 16; i < 16 + 16; i++){ /////////// relInfo.calculSlotCount
+			if(bufCurPageId.get(i) == 0){
+				freeSlot++;
+				if(freeSlot == 1){
+					freePos = i - 16;
+				}
+			}
+		}
+
+		for(int i = 0; i < freePos; i++){
+			cursor += 16; /////////////
+		}
+
+		record.writeToBuffer(bufCurPageId, cursor);
+		bufCurPageId.put(16 + freePos, (byte)1);
+		if(freeSlot == 1){
+			PageId prevPage = readPageIdFromPageBuffer(bufCurPageId, true);
+			PageId nextPage = readPageIdFromPageBuffer(bufCurPageId, false);
+
+			ByteBuffer prevBuf = bm.getPage(prevPage);
+			ByteBuffer nextBuf = bm.getPage(nextPage);
+
+			writePageIdToPageBuffer(pageId, headerPage, false);
+			writePageIdToPageBuffer(pageId, bufCurPageId, true); //////////////
+
+			if(!prevPage.equals(pageId)){ ///////////////
+				writePageIdToPageBuffer(nextPage, prevBuf, false);
+				writePageIdToPageBuffer(prevPage, nextBuf, true);
+			} else {
+				writePageIdToPageBuffer(nextPage, headerPage, true);
+				writePageIdToPageBuffer(pageId, nextBuf, true); //////////////
+				
+			}
+
+			bm.freePage(prevPage, true);
+			bm.freePage(nextPage, true);
+
+		}
+
+		bm.freePage(pageId, true);
+		bm.freePage(pageId, true); /////////////////
+
+
+		return new RecordId(pageId, freePos);
 		
 	}
 
-	//A VERIFIER
 	//Renvoie la liste des records stockés dans la pageId
 	public ArrayList<Record> getRecordsInDataPage(RelationInfo relInfo, PageId pageId) throws IOException{
 		BufferManager buffM = BufferManager.getLeBufferManager();
@@ -79,7 +122,6 @@ public class FileManager {
 		return records;
 	}
 
-	//A VERIFIER
 	public ArrayList<PageId> getAllPageId(RelationInfo relInfo) throws IOException{
 		HeaderPage hP = new HeaderPage(relInfo.getHeaderPageId());
 		ArrayList<PageId> allPageId = new ArrayList<PageId>();
@@ -94,16 +136,14 @@ public class FileManager {
 		return allPageId;
 	}
 	
-	//A VERIFIER 
 	//Insertion d'un record dans une relation
 	public RecordId InsertRecordIntoRelation (Record record) throws Exception {
 		int recSize = record.getWrittenSize();
 		return this.writeRecordToDataPage(record, getFreeDataPageId(record.getRelInfo(), recSize)); 
 	}
 
-	//A VERIFIER
 	//Lister tous les records dans une relation
-	public ArrayList<Record> GetAllRecords (RelationInfo relInfo) throws IOException{
+	public ArrayList<Record> GetAllRecords (RelationInfo relInfo) throws Exception{
 		ArrayList<Record> allRecord = new ArrayList<Record>();
 		ArrayList<PageId> allPageId = getAllPageId(relInfo);
 		while (allPageId.size()!=0) {
@@ -113,4 +153,27 @@ public class FileManager {
 		}
 		return allRecord;
 	}
+
+	public PageId readPageIdFromPageBuffer(ByteBuffer buf, boolean first)
+	{
+		int pageIdint = first? buf.getInt(0): buf.getInt(3);
+
+		int fileIdx = pageIdint / 10;
+		int pageIdx = pageIdint % 10;
+		PageId pageId = new PageId(fileIdx, pageIdx); 
+
+		return pageId;
+    }
+
+	public void writePageIdToPageBuffer(PageId pageId, ByteBuffer buf, boolean first)
+	{
+		String tmp = pageId.getFile() + "" + pageId.getPage();
+
+		int pageIdInt = Integer.valueOf(tmp);
+
+		if(first)
+			buf.putInt(0, pageIdInt);
+		else
+			buf.putInt(8, pageIdInt);
+    }
 }
